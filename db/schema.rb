b8a6e23c5ac7b3fb6a1f620c02f3c8c319651368
sap2411 <steven.parsons.us@gmail.com>
2020-09-12 14:16:21 -0700

    adds local postgres server

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_23_205811) do
+ActiveRecord::Schema.define(version: 2020_09_12_203124) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,6 +67,7 @@ ActiveRecord::Schema.define(version: 2020_03_23_205811) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "num_gear"
     t.index ["slug"], name: "index_itineraries_on_slug", unique: true
     t.index ["user_id"], name: "index_itineraries_on_user_id"
   end
